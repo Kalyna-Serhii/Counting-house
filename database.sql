34672a06bab3d285c90ae1147152b5bbe3cda061
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2023-07-03 13:43:03 +0300

    chore: added SQL

--- database.sql
+++ database.sql
@@ -1,7 +1,12 @@
-create TABLE person(
+create TABLE users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     surname VARCHAR(255),
-    apartment INTEGER,
+    phone VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255),
+    floor INTEGER,
+    room INTEGER,
+    role VARCHAR(255),
     avatar BYTEA
 );
