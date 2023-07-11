c7e8f640d60427e6eb8455e227839b0327d6fb96
baronin <baronin.yaroslav@gmail.com>
2023-07-11 20:13:30 +0300

    formatted

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
 const express = require('express')
-const cors = require('cors')
-const swaggerUI = require('swagger-ui-express')
-const userRouter = require('./routes/user')
-const app = express()
-const host = '127.0.0.1'
-const port = 3000
-const swaggerDocument = require('./swagger/swagger_autogen.json')
+const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const userRouter = require('./routes/user');
+const app = express();
+const host = '127.0.0.1';
+const port = 3000;
+const swaggerDocument = require('./swagger/swagger_autogen.json');
 const corsOptions = {
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200
-}
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
-app.use(express.json())
-app.use('/api', userRouter)
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use(express.json());
+app.use('/api', userRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, () => {
-    console.log(`Server listens http://${host}:${port}`)
-})
+  console.log(`Server listens http://${host}:${port}`);
+});
