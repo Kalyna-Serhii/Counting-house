c6e25490b7936b1c2f37aec466a04b8193cf0717
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2023-07-15 14:47:18 +0300

    chore: added Update button and inputs

--- client/src/index.js
+++ client/src/index.js
@@ -19,8 +19,14 @@ document.addEventListener('DOMContentLoaded', async function () {
       users.forEach((user) => {
         const tr = document.createElement('tr');
 
+        const id = document.createElement('td');
+        id.textContent = user.id;
+        tr.appendChild(id);
+
         const name = document.createElement('td');
-        name.textContent = user.name;
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = user.name; // Исправление здесь
+        name.appendChild(nameSpan);
         tr.appendChild(name);
 
         const surname = document.createElement('td');
@@ -55,10 +61,66 @@ document.addEventListener('DOMContentLoaded', async function () {
         avatar.textContent = user.avatar;
         tr.appendChild(avatar);
 
+        const editButton = document.createElement('td');
+        const editButtonInput = document.createElement('button');
+        editButtonInput.textContent = 'Редактировать';
+        editButton.appendChild(editButtonInput);
+        tr.appendChild(editButton);
+
+        editButtonInput.addEventListener('click', function () {
+          const nameInput = document.createElement('input');
+          const nameSpan = name.querySelector('span');
+          if (nameSpan && nameSpan.parentNode.contains(nameSpan)) {
+            nameInput.value = nameSpan.textContent;
+            name.removeChild(nameSpan);
+            name.appendChild(nameInput);
+            editButtonInput.textContent = 'Готово';
+            const editedName = nameInput.value;
+            fetch('http://127.0.0.1:3000/api/user/' + user.id, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                id: user.id,
+                name: editedName,
+                surname: 'Doe',
+                gender: 'man',
+                phone: '+380987554320',
+                password: '12345678f',
+                repeatPassword: '12345678f',
+                email: 'jojo@example.com',
+                floor: 1,
+                room: 1,
+                role: 'user',
+                avatar: '',
+              }),
+            })
+              .then((response) => {
+                if (response.ok) {
+                  createUserItemElement();
+                } else {
+                  console.log('Lil Peep');
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          } else {
+            if (nameInput.parentNode) {
+              nameInput.parentNode.removeChild(nameInput);
+            }
+            const newNameSpan = document.createElement('span');
+            newNameSpan.textContent = nameInput.value;
+            name.textContent = '';
+            name.appendChild(newNameSpan);
+            editButtonInput.textContent = 'Редактировать';
+            usersListElement.appendChild(tr);
+          }
+        });
         usersListElement.appendChild(tr);
       });
     };
     createUserItemElement();
-    console.log(users);
   }
 });
