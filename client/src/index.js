57bde09a3119dcccd7023c444fd6ff7f28e3f7e9
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2023-07-15 21:51:26 +0300

    chore: added Edit buttons

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 
-
 const URL = 'http://127.0.0.1:3000/api';
 
 async function getUsers() {
@@ -14,28 +13,41 @@ async function getUsers() {
 }
 
 document.addEventListener('DOMContentLoaded', async function () {
-  const tbody = document.querySelector("tbody");
+  const tbody = document.querySelector('tbody');
   const usersListElement = document.querySelector('#users');
-  const template = document.querySelector("#userRow");
+  const template = document.querySelector('#userRow');
+
   if (usersListElement) {
     const users = await getUsers();
+
     const createUserItemElement = () => {
-      users.forEach((user, index) => {
-        if ("content" in document.createElement("template")) {
+      for (const user of users) {
+        if ('content' in document.createElement('template')) {
           const clone = template.content.cloneNode(true);
-          let td = clone.querySelectorAll("td");
-          let tr = clone.querySelectorAll("tr");
-          const indexKeys = Object.keys(user);
-          for (let i = 0; i < indexKeys.length; i++) {
-            console.log('test', Object.keys(user)[i])
-            td[index].textContent = 1;
+          const td = clone.querySelectorAll('td');
+          const userValues = Object.values(user);
+
+          for (let i = 0; i < td.length; i++) {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'text');
+            input.setAttribute('readonly', '');
+            input.value = i >= 5 ? userValues[i + 1] : userValues[i];
+            td[i].textContent = '';
+            td[i].appendChild(input);
+
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Редактировать';
+            td[i].appendChild(editButton);
+
+            editButton.addEventListener('click', function () {
+              input.removeAttribute('readonly');
+              input.focus();
+            });
           }
           tbody.appendChild(clone);
         }
-      });
+      }
     };
     createUserItemElement();
-
-
   }
 });
