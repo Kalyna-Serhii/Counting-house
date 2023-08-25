4c54d848edd935db7a4497acb8b72747eb8670fd
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2023-08-25 18:06:19 +0300

    chore: added swagger to costs controller

--- server/src/controllers/cost-controller.js
+++ server/src/controllers/cost-controller.js
@@ -13,6 +13,36 @@ const CostController = {
     } catch (error) {
       next(error);
     }
+
+    // #swagger.tags = ['Costs']
+    // #swagger.summary = 'Get a list of costs'
+    // #swagger.description = 'Returns a list of all costs'
+    /* #swagger.responses[200] = {
+        schema: {
+            type: 'array'
+        },
+        examples: {
+            'application/json': [
+                {
+                  id: 1,
+                  title: "first title",
+                  comment: "first comment",
+                  date: "2023-09-24",
+                  sum: 2500,
+                  category: "Еда"
+              },
+              {
+                  id: 3,
+                  title: "Ремонт насоса",
+                  comment: null,
+                  date: "2023-03-21",
+                  sum: 1593,
+                  category: "Ремонт"
+              }
+            ]
+        }
+        } */
+    // #swagger.responses[500]
   },
 
   async createCostItem(req, res, next) {
@@ -23,6 +53,34 @@ const CostController = {
     } catch (error) {
       next(error);
     }
+
+    // #swagger.tags = ['Costs']
+    // #swagger.summary = 'Create a new cost item'
+    // #swagger.description = 'New cost item with the provided information'
+    /*  #swagger.parameters['obj'] = {
+               in: 'body',
+               description: 'CostModel object',
+               schema: {
+                    $title: "Ремонт насоса",
+                    comment: "Ремонт насоса, який зламався",
+                    $date: "2023-03-21",
+                    $sum: 1593,
+                    $category: "Ремонт"
+                }
+        } */
+    /* #swagger.responses[201] = {
+            schema: {
+                id: 4,
+                title: "Ремонт насоса",
+                comment: "Ремонт насоса, який зламався",
+                date: "2023-03-21",
+                sum: 1593,
+                category: "Ремонт"
+                }
+        }
+    } */
+    // #swagger.responses[400]
+    // #swagger.responses[500]
   },
 
   async updateCostItem(req, res, next) {
@@ -33,6 +91,33 @@ const CostController = {
     } catch (error) {
       next(error);
     }
+
+    // #swagger.tags = ['Costs']
+    // #swagger.summary = 'Update a fake user'
+    // #swagger.description = 'Updates a fake user by user id with the provided information'
+    /*  #swagger.parameters['obj'] = {
+                in: 'body',
+                description: 'CostModel object',
+                schema: {
+                    $title: "Ремонт насоса",
+                    comment: "Ремонт насоса, який зламався",
+                    $date: "2023-03-21",
+                    $sum: 1593,
+                    $category: "Ремонт"
+                }
+        } */
+    /* #swagger.responses[200] = {
+            schema: {
+                id: 4,
+                title: "Ремонт насосів",
+                comment: "Ремонт насосів, які зламались",
+                date: "2022-11-15",
+                sum: 2740,
+                category: "Ремонт"
+                }
+          } */
+    // #swagger.responses[400]
+    // #swagger.responses[500]
   },
 
   async deleteCostItem(req, res, next) {
@@ -42,6 +127,13 @@ const CostController = {
     } catch (error) {
       next(error);
     }
+
+    // #swagger.tags = ['Costs']
+    // #swagger.summary = 'Delete a cost item'
+    // #swagger.description = 'Deletes a cost item by cost id'
+    // #swagger.parameters['id'] = { description: 'CostModel id' }
+    // #swagger.responses[400]
+    // #swagger.responses[500]
   },
 };
 
